354851d15d2c70b194f21426501c3fdcd8605943
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2019-11-16 18:35:02 -0800

    adding componenets from multiple projects

--- chess960/client/src/components/GameWindow.js
+++ chess960/client/src/components/GameWindow.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
 import Chessboard from 'chessboardjsx'
-export class GameWindow extends Component {
+import Chess from 'chess.js'
+import io from 'socket.io-client'
+
+class GameWindow extends Component {
+
+  state = { 
+            color: 'white',
+            gameFen: "r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R" 
+          };
+
   render() {
+
+    componentDidMount() {    
+      socket.on('move', data => {
+        this.setState({ data })
+        game.move(data);
+      })
+    }
+
+    var socket = io();
+    var game = Chess();
+
     return (
       <container className="centered" style={containerStyle}>
-        <Chessboard position="r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R" />
+        <Chessboard 
+            position={this.state.gameFen}
+            orientation={this.state.color}
+            onDrop={(source, target) => {
+              let move = game.move({from: source, to: target});
+              if (game.move({from: source, to: target}) === null)  return 'snapback';
+              else socket.emit('move', move);
+            }}
+        />
       </container>
     )
   }
