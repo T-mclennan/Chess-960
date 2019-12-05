e330a24b604efe1b91a6e26ad47241bfd4143b51
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2019-12-04 18:13:41 -0800

    added chess images to the assets folder

---
+++ client/src/components/PlayerDetails.js
@@ -0,0 +1,17 @@
+/* TODO: This will be a component for displaying player information:
+*/
+
+
+import React, { Component } from 'react'
+
+export class GameCard extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default GameCard
