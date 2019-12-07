10fcd4b3f31bd8cb425a5466a8e06ea84d3ecc3a
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2019-12-06 20:31:15 -0800

    fixed minor issue with mongoose

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,11 @@
-// if (process.env.NODE_ENV === 'production') {
-//     module.exports = ('./prod');
-// } else {
-//     module.exports = ('./dev');
-// }
-
-module.exports = {
-    mongoURI: 'mongodb+srv://Tristan:mongodbpass@cluster0-5qw53.mongodb.net/Chess960?retryWrites=true&w=majority',
-    oAuthClient: '539980539315-2ga1urr1cdeai6a5edkkbpti3falgn39.apps.googleusercontent.com',
-    oAuthSecret: '7pZNx2J1br4pEfa_sCrdGuRs'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
 }
+
+// module.exports = {
+//     mongoURI: 'mongodb+srv://Tristan:mongodbpass@cluster0-5qw53.mongodb.net/Chess960?retryWrites=true&w=majority',
+//     oAuthClient: '539980539315-2ga1urr1cdeai6a5edkkbpti3falgn39.apps.googleusercontent.com',
+//     oAuthSecret: '7pZNx2J1br4pEfa_sCrdGuRs'
+// }
