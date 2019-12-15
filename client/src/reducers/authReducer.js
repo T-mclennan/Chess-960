d442ceadb8106daf1b466639c158bcebce848b4c
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2019-12-14 17:34:32 -0800

    implemented authReducer

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,55 @@
+import {
+  USER_LOADED,
+  USER_LOADING,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGOUT_SUCCESS,
+  LOGIN_FAIL,
+  REGISTER_FAIL,
+  REGISTER_SUCCESS
+} from "../actions/authTypes";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  isAuthenticated: null,
+  isLoading: false,
+  user: null
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case USER_LOADING:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        isLoading: false,
+        user: action.payload
+      };
+    case LOGIN_SUCCESS:
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        isLoading: false
+      };
+    case AUTH_ERROR:
+    case LOGIN_FAIL:
+    case LOGOUT_SUCCESS:
+    case REGISTER_FAIL:
+      return {
+        ...state,
+        token: null,
+        user: null,
+        isAuthenticated: false,
+        isLoading: false
+      };
+    default:
+      return state;
+  }
+}
