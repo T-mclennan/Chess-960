cfa211fd55b37bf1b51e1ab2b292f3451c072d14
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2019-12-15 22:05:31 -0800

    adding error handling for registration

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -34,7 +34,34 @@ export const loadPlayer = () => (dispatch, getState) => {
 };
 
 //Register User:
-export const register = ({ name, email, password }) => dispatch => {};
+export const register = ({ name, email, password }) => dispatch => {
+  //Headers:
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  //Request body:
+  const body = JSON.stringify({ name, email, password });
+
+  axios
+    .post("/api/players", body, config)
+    .then(res =>
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+      })
+    )
+    .catch(err => {
+      dispatch(
+        returnErrors(err.response.data, err.response.status, "REGISTER_FAIL")
+      );
+      dispatch({
+        type: REGISTER_FAIL
+      });
+    });
+};
 
 // Setup config/headers and token:
 export const tokenConfig = getState => {
