717ab2ab71f9dd90da5bce50e481aaf57fce5954
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2019-12-28 12:34:19 -0800

    setting up flexbox attributes for left sidebar

--- client/src/components/layout/Lobby/LeftSidebar.js
+++ client/src/components/layout/Lobby/LeftSidebar.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChessQueen, faSkating } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChessQueen,
+  faSkating
+  // faPlayCircle
+} from "@fortawesome/free-solid-svg-icons";
+
+import { faPlayCircle } from "@fortawesome/free-regular-svg-icons";
+import FontAwesome from "react-fontawesome";
+// import faStyles from "font-awesome/css/font-awesome.css";
 import "../css/lobby.css";
 // import "../../../src/App.css";
 
@@ -9,13 +17,40 @@ export class LeftSidebar extends Component {
   render() {
     return (
       <div className="leftsidebar">
-        <div style={{ fontSize: "2rem" }}>
-          <FontAwesomeIcon
-            icon={faChessQueen}
-            className="fa-2x fa-border fa-in"
-            data-fa-transform="grow-6"
-          />
-        </div>
+        <FontAwesomeIcon
+          icon={faPlayCircle}
+          className="icon far fa-2x fa-in"
+          data-far-transform="grow-6"
+          size="4x"
+        />
+
+        <FontAwesomeIcon
+          icon={faPlayCircle}
+          className="icon far fa-2x fa-in"
+          data-far-transform="grow-6"
+          size="4x"
+        />
+
+        <FontAwesomeIcon
+          icon={faPlayCircle}
+          className="icon far fa-2x fa-in"
+          // data-fa-transform="grow-5"
+          size="4x"
+        />
+
+        <FontAwesomeIcon
+          icon={faPlayCircle}
+          className="icon far fa-2x fa-in"
+          // data-fa-transform="grow-5"
+          size="4x"
+        />
+
+        <FontAwesomeIcon
+          icon={faPlayCircle}
+          className="icon far fa-2x fa-in"
+          data-fa-transform="grow-5"
+          size="4x"
+        />
       </div>
     );
   }
