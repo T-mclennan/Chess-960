e3903cbbd6d5f7d953c9e23e4cec1bd3239027b2
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2019-12-28 14:35:00 -0800

    styling buttons in the left sidebar

--- client/src/components/layout/Lobby/LeftSidebar.js
+++ client/src/components/layout/Lobby/LeftSidebar.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
+import { Button } from "reactstrap";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChessQueen,
-  faSkating
+  faSkating,
+  faExclamation,
+  faListOl,
+  faSearch,
+  faChessBoard
   // faPlayCircle
 } from "@fortawesome/free-solid-svg-icons";
 
-import { faPlayCircle } from "@fortawesome/free-regular-svg-icons";
+import { faPlayCircle, faUser } from "@fortawesome/free-regular-svg-icons";
 import FontAwesome from "react-fontawesome";
 // import faStyles from "font-awesome/css/font-awesome.css";
 import "../css/lobby.css";
@@ -17,43 +22,65 @@ export class LeftSidebar extends Component {
   render() {
     return (
       <div className="leftsidebar">
-        <FontAwesomeIcon
-          icon={faPlayCircle}
-          className="icon far fa-2x fa-in"
-          data-far-transform="grow-6"
-          size="4x"
-        />
+        <Button style={sidebarButton} onClick={{}}>
+          <FontAwesomeIcon
+            icon={faUser}
+            className="icon far fa-2x fa-in"
+            size="2x"
+          />
+          <div style={{ marginTop: "0.5rem" }}> Dashboard</div>
+        </Button>
 
-        <FontAwesomeIcon
-          icon={faPlayCircle}
-          className="icon far fa-2x fa-in"
-          data-far-transform="grow-6"
-          size="4x"
-        />
+        <Button style={sidebarButton} onClick={{}}>
+          <FontAwesomeIcon
+            icon={faPlayCircle}
+            className="icon far fa-2x fa-in"
+            size="2x"
+          />
+          <div style={{ marginTop: "0.5rem" }}>Quickplay</div>
+        </Button>
 
-        <FontAwesomeIcon
-          icon={faPlayCircle}
-          className="icon far fa-2x fa-in"
-          // data-fa-transform="grow-5"
-          size="4x"
-        />
+        <Button style={sidebarButton} onClick={{}}>
+          <FontAwesomeIcon
+            icon={faChessBoard}
+            className="icon far fa-2x fa-in"
+            size="2x"
+          />
+          <div style={{ marginTop: "0.5rem" }}>New Game</div>
+        </Button>
 
-        <FontAwesomeIcon
-          icon={faPlayCircle}
-          className="icon far fa-2x fa-in"
-          // data-fa-transform="grow-5"
-          size="4x"
-        />
+        <Button style={sidebarButton} onClick={{}}>
+          <FontAwesomeIcon
+            icon={faSearch}
+            className="icon far fa-2x fa-in"
+            size="2x"
+          />
+          Find Game
+        </Button>
 
-        <FontAwesomeIcon
-          icon={faPlayCircle}
-          className="icon far fa-2x fa-in"
-          data-fa-transform="grow-5"
-          size="4x"
-        />
+        <Button style={sidebarButton} onClick={{}}>
+          <FontAwesomeIcon
+            icon={faListOl}
+            className="icon far fa-2x fa-in"
+            size="2x"
+          />
+          Scoreboard
+        </Button>
       </div>
     );
   }
 }
+
+const sidebarButton = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "black",
+  padding: "1rem",
+  width: "8rem",
+  margin: "auto",
+  borderColor: "black"
+};
 
 export default LeftSidebar;
