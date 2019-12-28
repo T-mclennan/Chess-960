adbcd3316894310026ea985a099e1e012aa32a7c
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2019-12-27 23:46:05 -0800

    adding fontawesome react to sidebar of lobby

--- client/src/components/layout/Lobby/LeftSidebar.js
+++ client/src/components/layout/Lobby/LeftSidebar.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChessQueen, faSkating } from "@fortawesome/free-solid-svg-icons";
 import "../css/lobby.css";
 // import "../../../src/App.css";
 
 export class LeftSidebar extends Component {
   render() {
-    return <div className="leftsidebar"> </div>;
+    return (
+      <div className="leftsidebar">
+        <div style={{ fontSize: "2rem" }}>
+          <FontAwesomeIcon
+            icon={faChessQueen}
+            className="fa-2x fa-border fa-in"
+            data-fa-transform="grow-6"
+          />
+        </div>
+      </div>
+    );
   }
 }
 
