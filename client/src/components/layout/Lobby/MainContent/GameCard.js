359274a34628418695530c181f5a7d2322115667
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-01-10 23:38:06 -0800

    added player details to the GameCard component

--- client/src/components/layout/Lobby/MainContent/GameCard.js
+++ client/src/components/layout/Lobby/MainContent/GameCard.js
@@ -1,21 +1,59 @@
 import React, { Component } from "react";
 import { Button, Card, CardText } from "reactstrap";
 import Chessboard from "chessboardjsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCircle } from "@fortawesome/free-regular-svg-icons";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
 import "../../css/Dashboard.css";
 
 class GameCard extends Component {
   render() {
     return (
-      <Card body outline color="secondary">
+      //   <a>
+      <Card body outline color="secondary" style={cardStyle}>
         <div className="cardBody">
           <Chessboard position={this.props.fen} width={200} draggable={false} />
-          <CardText>{this.props.white}</CardText>
-          <CardText>{this.props.black}</CardText>
+          <div className="playerTitle">
+            <div>
+              <FontAwesomeIcon
+                icon={faCircle}
+                size="xs"
+                style={{ marginRight: "0.3rem" }}
+              />
+              {this.props.white}
+            </div>
+            <div>
+              <FontAwesomeIcon
+                icon={faCircle}
+                size="xs"
+                style={{
+                  //   marginLeft: "0.5rem",
+                  marginRight: "0.3rem",
+                  color: "black"
+                }}
+              />
+              {this.props.black}
+            </div>
+          </div>
         </div>
         {/* <CardTitle>Special Title Treatment</CardTitle> */}
       </Card>
+      //   </a>
     );
   }
 }
+
+{
+  /* <i class="far fa-circle"></i> */
+}
+
+const cardStyle = {
+  backgroundColor: "darkgrey",
+  margin: "0.6rem",
+  flex: "0 22% ",
+  minWidth: "13.5rem",
+  padding: "0.4rem"
+  /* flex: 0 1 calc(25% - 1em) !important; */
+};
 
 export default GameCard;
