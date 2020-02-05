8b20e040507c5bdbe4145e21f778fc6c145464d3
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-02-04 16:26:48 -0800

    exploring heroku issues around websocket usage

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ import * as io from "socket.io-client";
 import FourOhFour from "./components/layout/fourohfour";
 
 const port = process.env.PORT || "http://127.0.0.1:5000";
-const socket = io(port, { pingTimeout: 30000 });
+const socket = io();
 // const socket = io();
 
 socket.on("disconnect", reason => {
