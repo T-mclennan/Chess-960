f19acc2346b35f46aa8bdd4ded395f6b67c00171
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-02-07 16:43:08 -0800

    cleaned up socket-context.js

--- client/src/socket-context.js
+++ client/src/socket-context.js
@@ -3,20 +3,3 @@ import React from "react";
 const SocketContext = React.createContext();
 
 export default SocketContext;
-
-// TODO:
-// Portion to go in Lobby:
-// Pass socket into all underlying components:
-
-// import SocketContext from './socket-context'
-// import * as io from 'socket.io-client'
-
-// const socket = io()
-
-// const App = props => (
-//   <SocketContext.Provider value={socket}>
-//     <YourChildComponent />
-//   </SocketContext.Provider>
-// )
-
-//https://itnext.io/how-to-use-a-single-instance-of-socket-io-in-your-react-app-6a4465dcb398
