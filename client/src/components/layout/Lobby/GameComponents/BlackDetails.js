153a65572f6db405c7353df3afe3e0f266ab5ae1
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-03-10 19:08:09 -0700

    added missing alt prop inside white and black game components

--- client/src/components/layout/Lobby/GameComponents/BlackDetails.js
+++ client/src/components/layout/Lobby/GameComponents/BlackDetails.js
@@ -2,32 +2,32 @@
 // for the black player:
 */
 
-import React, { Component } from "react";
-import blackPawn from "../../../../assets/images/bP.png";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import blackPawn from '../../../../assets/images/bP.png';
+import { connect } from 'react-redux';
 
 export class BlackDetails extends Component {
   render() {
     const { turn, color } = this.props.game;
-    const borderColor = color === turn ? "#ffd5d3" : "silver";
+    const borderColor = color === turn ? '#ffd5d3' : 'silver';
     const pawn = {
       WebkitFilter: `drop-shadow(1px 1px 0 ${borderColor}) drop-shadow(-1px -1px 0 ${borderColor})`,
-      filter: `drop-shadow(1px 1px 0 ${borderColor}) drop-shadow(-1px -1px 0 ${borderColor})`
+      filter: `drop-shadow(1px 1px 0 ${borderColor}) drop-shadow(-1px -1px 0 ${borderColor})`,
     };
 
     return (
-      <div className="playerDetails">
-        <div className="clock"></div>
-        <div className="peice">
-          <img className="cover" style={pawn} src={blackPawn} />
+      <div className='playerDetails'>
+        <div className='clock'></div>
+        <div className='peice'>
+          <img className='cover' style={pawn} src={blackPawn} alt={''} />
         </div>
-        <div className="infoBox">
+        <div className='infoBox'>
           <h2>
-            {" "}
+            {' '}
             {this.props.game.black ? (
               this.props.game.black
             ) : (
-              <div className="emptyStyling">Waiting for opponent</div>
+              <div className='emptyStyling'>Waiting for opponent</div>
             )}
           </h2>
           {/* <h3>{this.props.rating}</h3> */}
@@ -40,7 +40,7 @@ export class BlackDetails extends Component {
 }
 
 const mapStateToProps = state => ({
-  game: state.game
+  game: state.game,
 });
 
 export default connect(mapStateToProps, {})(BlackDetails);
--- client/src/components/layout/Lobby/GameComponents/WhiteDetails.js
+++ client/src/components/layout/Lobby/GameComponents/WhiteDetails.js
@@ -1,16 +1,16 @@
 /* TODO: This will be a component for displaying player information:
  */
 
-import React, { Component } from "react";
-import whitePawn from "../../../../assets/images/wP.png";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import whitePawn from '../../../../assets/images/wP.png';
+import { connect } from 'react-redux';
 
 export class WhiteDetails extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      rating: ""
+      rating: '',
     };
   }
 
@@ -20,25 +20,25 @@ export class WhiteDetails extends Component {
   }
   render() {
     const { turn, color } = this.props.game;
-    const borderColor = color === turn ? "#ffd5d3" : "silver";
+    const borderColor = color === turn ? '#ffd5d3' : 'silver';
     const pawn = {
       WebkitFilter: `drop-shadow(1px 1px 0 ${borderColor}) drop-shadow(-1px -1px 0 ${borderColor})`,
-      filter: `drop-shadow(1px 1px 0 ${borderColor}) drop-shadow(-1px -1px 0 ${borderColor})`
+      filter: `drop-shadow(1px 1px 0 ${borderColor}) drop-shadow(-1px -1px 0 ${borderColor})`,
     };
 
     return (
-      <div className="playerDetails">
-        <div className="clock"></div>
-        <div className="peice">
-          {" "}
-          <img className="cover" style={pawn} src={whitePawn} />
+      <div className='playerDetails'>
+        <div className='clock'></div>
+        <div className='peice'>
+          {' '}
+          <img className='cover' style={pawn} src={whitePawn} alt={''} />
         </div>
-        <div className="infoBox">
+        <div className='infoBox'>
           <h2>
             {this.props.game.white ? (
               this.props.game.white
             ) : (
-              <div className="emptyStyling">Waiting for opponent</div>
+              <div className='emptyStyling'>Waiting for opponent</div>
             )}
           </h2>
           {/* <h3>{this.props.rating}</h3> */}
@@ -51,7 +51,7 @@ export class WhiteDetails extends Component {
 }
 
 const mapStateToProps = state => ({
-  game: state.game
+  game: state.game,
 });
 
 export default connect(mapStateToProps, {})(WhiteDetails);
